37946fc7c5622fac24cad70143369b12a0995ed0
rodolfo cancino <rodolfo.cancino.salazar@gmail.com>
2026-03-25 17:51:49 -0300

    Cambios en package tests y pages

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,23 +1,8 @@
 package tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+
 
 public class LoginTest {
 
-    WebDriver driver;
 
-    By username = By.id("username");
-    By password = By.id("password");
-    By loginBtn = By.id("login");
-
-    public LoginTest(WebDriver driver){
-        this.driver = driver;
-    }
-
-    public void login(String user, String pass){
-        driver.findElement(username).sendKeys(user);
-        driver.findElement(password).sendKeys(pass);
-        driver.findElement(loginBtn).click();
-    }
 }
